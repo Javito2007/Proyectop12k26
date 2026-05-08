3a65e892c20cc5d2b155b6bd6e6c42bf4d14eeaf
Fernando Andrino <aandrinog@miumg.edu.gt>
2026-05-07 19:42:06 -0600

    Axel Andrino: Constancias Bancarias

---
+++ proyectop12k26/include/Bancos.h
@@ -0,0 +1,31 @@
+#ifndef BANCOS_H
+#define BANCOS_H
+
+#include <string>
+#include "GeneradorConstancias.h"
+
+using namespace std;
+
+class Bancos
+{
+    public:
+        Bancos();
+        virtual ~Bancos();
+        bool InfoTransferencia(string nombreBanco, string nombreCliente, long long numeroTarjeta);
+        bool InfoPagoPlanilla(string nombreBanco, string nombreCliente, int idCuenta);
+        bool procesoTransferencia(long long numeroTarjeta, double monto, double saldo);
+        bool procesoPagoPlanilla(int idCuenta, double monto, double saldoCuenta);
+
+        bool crearCuenta(string nombreCliente, double monto, string nombreBanco);
+        bool guardarCuenta(int idCuenta, string nombreCliente, double saldo, double movimiento, long long numeroTarjeta, string nombreBanco);
+        int generadorTarjetasCuentas();
+
+    private:
+        string nombreCliente;
+        string nombreBanco;
+        double saldo;
+        double movimiento;
+        int idCuenta;
+        long long numeroTarjeta;
+};
+#endif
--- proyectop12k26/include/GeneradorConstancias.h
+++ proyectop12k26/include/GeneradorConstancias.h
@@ -11,26 +11,30 @@ class GeneradorConstancias {
         GeneradorConstancias();
         virtual ~GeneradorConstancias();
 
-
         void setNombre(string n);
+        void setCarnet(string c);
         void setMonto(float m);
+        void setEstado(int e);
         void setTipoPersona(int t);
 
         string getNombre();
+        string getCarnet();
         float getMonto();
+        string getTextoEstado();
         int getTipoPersona();
 
         void entregarConstancia();
 
     private:
         string nombre;
+        string carnet;
         float monto;
+        int estado;
         int tipoPersona;
 
         void imprimirEncabezadoBanco();
         void constanciaAlumno();
         void constanciaDocente();
-        void constanciaEmpleado();
 };
 
 #endif
